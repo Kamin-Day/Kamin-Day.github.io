72a62b893f65ecf5e0de0d948f42d702ae01375b
Kamin Day <kaminday@gmail.com>
2017-10-25 15:52:20 -0500

    added to coverletter, should have reviewed

--- btscripts.js
+++ btscripts.js
@@ -1,5 +1,4 @@
 window.addEventListener("load", function(){
-	
 
 	resumeView = document.getElementsByClassName("resume")[0];
 	aboutView = document.getElementsByClassName("about")[0];
@@ -8,12 +7,18 @@ window.addEventListener("load", function(){
 	aboutButton.addEventListener("click", function(){
 		resumeView.style.display = "none";
 		aboutView.style.display = "block";
+		resumeButton.style.color = "rgb(244,242,240)";
+		aboutButton.style.color = "rgb(119, 211, 239)";
+
 	});
 
 	var resumeButton = document.getElementById("myresume");
 	resumeButton.addEventListener("click", function(){
 		resumeView.style.display = "block";
 		aboutView.style.display = "none";
+		resumeButton.style.color = "rgb(119, 211, 239)";
+		aboutButton.style.color = "rgb(244,242,240)";
 	});
 	
 });
+
